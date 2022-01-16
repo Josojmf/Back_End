c958b5487c8574cd2a195bf80350f78a5e7e3bd6
Josojmf <joso.jmf@gmail.com>
2022-01-16 18:27:19 +0100

    Add files via upload

---
+++ TrabajoFinal/src/schema.ts
@@ -0,0 +1,39 @@
+import { ApolloError, ApolloServer, gql } from "apollo-server";
+
+export const typeDefs = gql`
+
+type User {
+    id: ID!
+    email: String!
+    pwd: String!
+    token: String
+}
+type Chat{
+    name:String!
+    user:Int!
+}
+type Query{
+    getChats:[String]
+}
+type Subscription {
+    postCreated(chat:String!,token:String!): Post!
+   
+  }
+type Post{
+    email : String!
+    comment : String!
+}
+
+
+
+type Mutation{
+    LogIn(email: String!, pwd: String!): Boolean!
+    LogOut: String
+    SignIn(email: String!, pwd: String!): User!
+    SignOut:Boolean!
+    createPost(email: String!, comment: String!): Post!
+    quit:String
+    
+}
+
+`
